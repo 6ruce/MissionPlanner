2138fccb8e10e60301adccfe73cfc6ec66e76b4b
Vitalii Braslavskyi <vitalii.braslavskyi@grammarly.com>
2024-03-02 19:44:25 +0200

    GeoMesh prototype.

---
+++ Plugins/GeoMesh/GMapMarkerGeoMesh.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Serialization;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using MissionPlanner.Utilities;
+
+namespace GeoMesh
+{
+    public class GMapMarkerGeoMesh : GMapMarker
+    {
+        private readonly float _groundCourse;
+        private const int _radius = 100;
+
+        public GMapMarkerGeoMesh(PointLatLng pos, float groundCourse) : base(pos)
+        {
+            _groundCourse = groundCourse;
+            Size = new Size(2 * _radius, 2 * _radius);
+        }
+
+        public override void OnRender(IGraphics g)
+        {
+            var temp = g.Transform;
+            
+            g.TranslateTransform(LocalPosition.X, LocalPosition.Y);
+            g.RotateTransform(-Overlay.Control.Bearing);
+            
+            double width =
+                (Overlay.Control.MapProvider.Projection.GetDistance(Overlay.Control.FromLocalToLatLng(0, 0),
+                     Overlay.Control.FromLocalToLatLng(Overlay.Control.Width, 0)) * 1000.0);
+
+            double scale = Overlay.Control.Width / width;
+            
+            DrawMeshCircle(_radius, scale, g);
+            DrawMeshCircle(_radius / 2, scale, g);
+            DrawMeshCircle(_radius * 2, scale, g);
+            
+            g.Transform = temp;
+        }
+
+        private void DrawMeshCircle(int radius, double scale, IGraphics g)
+        {
+            var scaledRadius = radius * (float) scale;
+
+            g.DrawEllipse(
+                new Pen(Color.HotPink, 2),
+                -scaledRadius, -scaledRadius,
+                Math.Abs(scaledRadius) * 2, Math.Abs(scaledRadius) * 2);
+        }
+    }
+}
---
+++ Plugins/GeoMesh/GeoMeshPlugin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using MissionPlanner;
+using MissionPlanner.GCSViews;
+
+namespace GeoMesh
+{
+    public class GeoMeshPlugin : MissionPlanner.Plugin.Plugin
+    {
+        public override string Name { get; } = "GeoMesh";
+        public override string Version { get; } = "0.1";
+        public override string Author { get; } = "Cherub Team";
+        public override bool Init()
+        {
+            // Separate overlay to hold the mesh. It's addede specifically to gMapControl1 as it represents the map 
+            // control that works during the flight.
+            var overlay = new GMapOverlay("positions");
+            FlightData.instance.gMapControl1.Overlays.Add(overlay);
+
+            // Frequency of the loop iterations
+            loopratehz = 1f;
+                        
+            return true;
+        }
+
+        public override bool Loaded()
+        {
+            return true;
+        }
+
+        public override bool Loop()
+        {
+            var meshOverlay = Host.FDGMapControl.Overlays.FirstOrDefault(o => o.Id == "positions");
+            
+            // Thing that know how to render the mesh itself
+            var geoMeshMarker = new GMapMarkerGeoMesh(Host.cs.Location, Host.cs.groundcourse);
+            
+            // Marker should be re-added each iteration to update its position
+            meshOverlay?.Markers.Clear();
+            meshOverlay?.Markers.Add(geoMeshMarker);
+
+            return base.Loop();
+        }
+
+        public override bool SetupUI(int gui = 0, object data = null)
+        {
+            return base.SetupUI(gui, data);
+        }
+
+        public override bool Exit()
+        {
+            return true;
+        }
+    }
+}
