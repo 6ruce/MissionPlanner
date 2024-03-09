ac2fd3f6d6b4b46c56d507c128507affcebb2aaa
Roman Yavorskyi <roman.yavorskyi@grammarly.com>
2024-03-09 18:59:42 +0200

    very rough scaling implementation

--- Plugins/GeoMesh/GMapMarkerGeoMesh2.cs
+++ Plugins/GeoMesh/GMapMarkerGeoMesh2.cs
@@ -18,6 +18,8 @@ namespace GeoMesh
 
         public override void OnRender(IGraphics g)
         {
+            var unitSize = CalculateUnitSize();
+            
             var temp = g.Transform;
 
             // Instead of calculating the angled lines coordinates we draw the vertical line and rotate the control
@@ -31,10 +33,10 @@ namespace GeoMesh
                 
                 g.DrawLine(
                     new Pen(Color.FromArgb(180, Color.HotPink), penWidth),
-                    0, 
-                    -Overlay.Control.Height / 2, 
+                    -Overlay.Control.Width / 2, 
                     0,
-                    Overlay.Control.Height / 2);
+                    Overlay.Control.Width / 2,
+                    0);
                 g.RotateTransform(10);
             }
             
@@ -42,30 +44,75 @@ namespace GeoMesh
             {
                 // Each fifth circle should be bold
                 var penWidth = i % 5 == 0
-                    ? 3
-                    : 1;
+                    ? 5
+                    : 3;
                 
-                DrawMeshCircle(Overlay.Control.Height / 20 * i, 1, penWidth, g);
+                DrawMeshCircle(unitSize.unitSizeInPixels * i, 1, penWidth, g);
             }
-            //g.TranslateTransform(LocalPosition.X, LocalPosition.Y);
-            //g.RotateTransform(-Overlay.Control.Bearing);
             
-            double width =
-                (Overlay.Control.MapProvider.Projection.GetDistance(Overlay.Control.FromLocalToLatLng(0, 0),
-                     Overlay.Control.FromLocalToLatLng(Overlay.Control.Width, 0)) * 1000.0);
-
-            double scale = Overlay.Control.Width / width;
+            g.TranslateTransform(LocalPosition.X, LocalPosition.Y);
             
             g.Transform = temp;
+            
+            g.DrawString(
+                $"Current unit size: {unitSize.unitSize} meters",
+                new Font("Arial", 16),
+                new SolidBrush(Color.Crimson),
+                0,
+                0);
         }
 
         private void DrawMeshCircle(int radius, double scale, float penWidth, IGraphics g)
         {
             var scaledRadius = radius * (float) scale;
             g.DrawEllipse(
-                new Pen(Color.FromArgb(180, Color.HotPink), penWidth),
+                new Pen(Color.FromArgb(255, Color.HotPink), penWidth),
                 -scaledRadius, -scaledRadius,
                 Math.Abs(scaledRadius) * 2, Math.Abs(scaledRadius) * 2);
+        }
+
+        private (double unitSize, int unitSizeInPixels) CalculateUnitSize()
+        {
+            
+            //calculate visible distance in meters on screen between lowest and highest point
+            double heightInMeters =
+                (Overlay.Control.MapProvider.Projection.GetDistance(Overlay.Control.FromLocalToLatLng(Overlay.Control.Width / 2, 0),
+                    Overlay.Control.FromLocalToLatLng(Overlay.Control.Width / 2, Overlay.Control.Height)) * 1000.0);
+
+            // calculate the desired unit size;
+            var desiredUnitSize = heightInMeters / 20;
+            
+            // find the closest lower unit size that starts from 5 or 10 in km
+            var unitSize = 0.1;
+            var diff = desiredUnitSize - unitSize;
+            while (true)
+            {
+                if (diff <= 0.1)
+                {
+                    break;
+                }
+                
+                var nextUnitSize = unitSize * 5;
+                var nextDiff = desiredUnitSize - nextUnitSize;
+                diff = nextDiff;
+                unitSize = nextUnitSize;
+                if (diff <= 0.1)
+                {
+                    break;
+                }
+
+                nextUnitSize = unitSize * 2;
+                nextDiff = desiredUnitSize - nextUnitSize;
+
+                diff = nextDiff;
+                unitSize = nextUnitSize;
+            }
+
+            // assume that distance units on maps are even (not entirely true because of surface curvature) and find
+            // pixel unit we want to use
+            var unitSizeInPixels = unitSize /  (heightInMeters / Overlay.Control.Height);
+
+            return (unitSize, Convert.ToInt32(unitSizeInPixels));
         }
     }
 }
